9e1a40fa9426fb6c3b2c06709576d62a5a33bacf
st0p <nicholastung47@gmail.com>
2018-07-05 14:09:39 -0400

    Add printBoard method and CLI program for early interaction and testing

---
+++ src/PlayTicTacToeCLI.java
@@ -0,0 +1,8 @@
+//Used for testing the TicTacToe class in development before implementing with Swing
+public class PlayTicTacToeCLI {
+    public static void main (String [] args) {
+        TicTacToe game = new TicTacToe();
+
+        game.printBoard();
+    }
+}
--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,3 +1,35 @@
 public class TicTacToe {
-    
+    private int [][] board;
+
+    public TicTacToe () {
+        board = new int[][] {{0 , 0 , 0},{0 , 0 , 0},{0 , 0 , 0}};
+    }
+
+    public void printBoard () {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j <= 2; j++) {
+                if (board[i][j] == 0 || board[i][j] == 1 || board[i][j] == 2) {
+                    if (board[i][j] == 0) {
+                        System.out.print(" ");
+                    }
+                    if (board[i][j] == 1) {
+                        System.out.print("X");
+                    }
+                    if (board[i][j] == 2) {
+                        System.out.print("O");
+                    }
+                }
+                if (j < 2) {
+                    System.out.print(" |");
+                }
+            }
+            if (i < 2) {
+                System.out.println();
+                System.out.println("---------");
+
+            }
+        }
+    }
+
+
 }
