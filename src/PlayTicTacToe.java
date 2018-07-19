4a23dadc6ded20d09849aba5a22bc4ea452d7708
st0p47 <nicholastung47@gmail.com>
2018-07-18 18:55:22 -0700

    Make window appear with 9 buttons and restart button (no image yet)

--- src/PlayTicTacToe.java
+++ src/PlayTicTacToe.java
@@ -3,10 +3,8 @@ import javax.swing.*;
 
 public class PlayTicTacToe {
     public static void addComponentsToPane(Container pane) {
-        pane = new JPanel(new GridBagLayout());
+        pane.setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
-
-        pane.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
 
         JButton gridButtons [] = new JButton[9];
         JButton restartButton = new JButton();
@@ -21,6 +19,7 @@ public class PlayTicTacToe {
             for (int j = 0; j < 3; j++) {
                 c.fill = GridBagConstraints.BOTH;
                 c.weightx = 0.5;
+                c.weighty = 0.5;
                 c.gridx = i;
                 c.gridy = j;
                 pane.add(gridButtons[buttonCount], c);
@@ -28,18 +27,29 @@ public class PlayTicTacToe {
             }
         }
 
+        //restartButton
+        c.fill = GridBagConstraints.BOTH;
+        c.ipady = 0;
+        c.weightx = 0.5;
+        c.weighty = 0.5;
+        c.anchor = GridBagConstraints.LAST_LINE_END;
+        c.insets = new Insets(20,0,0,0);  //top padding
+        c.gridx = 2;
+        c.gridwidth = 1;
+        c.gridy = 3;
+        pane.add(restartButton, c);
     }
     private static void createAndShowGUI() {
         //copied from GridBagLayout Tutorial haHAA
         //Create and set up the window.
-        JFrame frame = new JFrame("GridBagLayoutDemo");
+        JFrame frame = new JFrame("TicTacToe maybe");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         //Set up the content pane.
         addComponentsToPane(frame.getContentPane());
 
         //Display the window.
-        frame.pack();
+        frame.setSize(300 , 420);
         frame.setVisible(true);
     }
     public static void main(String[] args) {
